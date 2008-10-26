585d33bbde40a958c787943c5f0c9212f4c1d4aa
dmoonfire <dmoonfire@84af6989-d30b-0410-891c-c1f6a83ca0db>
2008-10-26 23:23:05 +0000

    Added an SVG loading example.
    git-svn-id: http://svn.mfgames.com/BooGame/trunk@913 84af6989-d30b-0410-891c-c1f6a83ca0db

--- BooGameExample/GemsMode.cs
+++ BooGameExample/GemsMode.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.IO;
 
 using BooGame;
 
@@ -6,6 +9,8 @@ using MfGames.Numerics;
 using MfGames.Scene2;
 using MfGames.Scene2.Collections;
 using MfGames.Scene2.Tao.OpenGL;
+
+using Svg;
 
 namespace BooGameExample
 {
@@ -32,6 +37,22 @@ namespace BooGameExample
 			imageKey = DevILImageLoader.Load(new FileInfo("Images/Gem Green.png"));
 			image = new ImageNode<float>(imageKey);
 			image.Point = new Point2<float>(60, 90);
+			nodes.Add(image);
+
+			// Load the baby squid image
+			SvgDocument svg = SvgDocument.Open("Images/Baby Squid.svg");
+			Bitmap bitmap = new Bitmap(400, 400);
+			SvgRenderer renderer = SvgRenderer.FromImage(bitmap);
+			renderer.ScaleTransform(10, 10);
+			renderer.TextRenderingHint = TextRenderingHint.AntiAlias;
+			renderer.TextContrast = 1;
+			renderer.PixelOffsetMode = PixelOffsetMode.Half;
+			svg.Draw(renderer);
+			renderer.Save();
+
+			imageKey = SystemImageLoader.Load(bitmap);
+			image = new ImageNode<float>(imageKey);
+			image.Point = new Point2<float>(200, 10);
 			nodes.Add(image);
 		}
 		#endregion Constructors
